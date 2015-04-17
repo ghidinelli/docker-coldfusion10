262eef124356fda6e7baaa706ac7fbe1cbc92ff2
Brian Ghidinelli <brian-github@vfive.com>
2015-04-17 22:44:27 +0000

    Add custom jvm.config support

--- build/install/install-cf10.sh
+++ build/install/install-cf10.sh
@@ -10,6 +10,9 @@
 # use our heavily-locked down/restricted web.xml file
 cp -f /tmp/web.xml /opt/coldfusion10/cfusion/wwwroot/WEB-INF/web.xml
 
+# use our jvm.config file (which also swaps jvm out to openjdk 7)
+cp -f /tmp/jvm.config /opt/coldfusion10/cfusion/bin
+
 # change wwwroot to be /var/www	
 sed -i 's|</Host>|<Context path="/" docBase="/var/www" WorkDir="/opt/coldfusion10/cfusion/runtime/conf/Catalina/localhost/tmp" aliases="/CFIDE=/opt/coldfusion10/cfusion/wwwroot/CFIDE,/WEB-INF=/opt/coldfusion10/cfusion/wwwroot/WEB-INF"></Context></Host>|' /opt/coldfusion10/cfusion/runtime/conf/server.xml
 
